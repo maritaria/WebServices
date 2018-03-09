cb8b340cb447143bce0d0f88fd31e41bf374fc38
maritaria <bramkamies@gmail.com>
2018-03-09 16:20:45 +0100

    Updated patient Record

--- mcl.server/src/nl/mcl/services/db/PatientRecord.java
+++ mcl.server/src/nl/mcl/services/db/PatientRecord.java
@@ -1,7 +1,18 @@
 package nl.mcl.services.db;
 
+import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
 @DatabaseTable(tableName = "patients")
 public class PatientRecord {
+	@DatabaseField(id=true)
+	public String id;
+	@DatabaseField
+	public String name;
+	@DatabaseField
+	public String phonenumber;
+	@DatabaseField
+	public String email;
+	@DatabaseField
+	public String address;
 }
