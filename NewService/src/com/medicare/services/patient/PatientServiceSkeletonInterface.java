f52b9ff58e05227f71fb76d21b55b3346e437082
maritaria <bramkamies@gmail.com>
2018-03-12 11:31:50 +0100

    Formatting

--- NewService/src/com/medicare/services/patient/PatientServiceSkeletonInterface.java
+++ NewService/src/com/medicare/services/patient/PatientServiceSkeletonInterface.java
@@ -5,54 +5,40 @@
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:03:39 GMT)
  */
-    package com.medicare.services.patient;
-    /**
-     *  PatientServiceSkeletonInterface java skeleton interface for the axisService
-     */
-    public interface PatientServiceSkeletonInterface {
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                    * @param person
-             * @throws RegisterFault : 
-         */
+package com.medicare.services.patient;
 
-        
-                public com.medicare.types.PatientE register
-                (
-                  com.medicare.types.PersonE person
-                 )
-            throws RegisterFault;
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                    * @param notify
-             * @throws NotifyFaultException : 
-         */
+/**
+ * PatientServiceSkeletonInterface java skeleton interface for the axisService
+ */
+public interface PatientServiceSkeletonInterface {
 
-        
-                public void notify
-                (
-                  com.medicare.services.patient.Notify notify
-                 )
-            throws NotifyFaultException;
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-         */
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param person
+	 * @throws RegisterFault
+	 *             :
+	 */
 
-        
-                public com.medicare.types.Patients getAll
-                (
-                  
-                 )
-            ;
-        
-         }
-    
+	public com.medicare.types.PatientE register(com.medicare.types.PersonE person) throws RegisterFault;
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 * @param notify
+	 * @throws NotifyFaultException
+	 *             :
+	 */
+
+	public void notify(com.medicare.services.patient.Notify notify) throws NotifyFaultException;
+
+	/**
+	 * Auto generated method signature
+	 * 
+	 */
+
+	public com.medicare.types.Patients getAll(
+
+	);
+
+}
--- NewService/src/com/medicare/services/patient/RegisterFault.java
+++ NewService/src/com/medicare/services/patient/RegisterFault.java
@@ -8,36 +8,33 @@
 
 package com.medicare.services.patient;
 
-public class RegisterFault extends java.lang.Exception{
+public class RegisterFault extends java.lang.Exception {
 
-    private static final long serialVersionUID = 1520850479397L;
-    
-    private com.medicare.types.Error faultMessage;
+	private static final long serialVersionUID = 1520850479397L;
 
-    
-        public RegisterFault() {
-            super("RegisterFault");
-        }
+	private com.medicare.types.Error faultMessage;
 
-        public RegisterFault(java.lang.String s) {
-           super(s);
-        }
+	public RegisterFault() {
+		super("RegisterFault");
+	}
 
-        public RegisterFault(java.lang.String s, java.lang.Throwable ex) {
-          super(s, ex);
-        }
+	public RegisterFault(java.lang.String s) {
+		super(s);
+	}
 
-        public RegisterFault(java.lang.Throwable cause) {
-            super(cause);
-        }
-    
+	public RegisterFault(java.lang.String s, java.lang.Throwable ex) {
+		super(s, ex);
+	}
 
-    public void setFaultMessage(com.medicare.types.Error msg){
-       faultMessage = msg;
-    }
-    
-    public com.medicare.types.Error getFaultMessage(){
-       return faultMessage;
-    }
+	public RegisterFault(java.lang.Throwable cause) {
+		super(cause);
+	}
+
+	public void setFaultMessage(com.medicare.types.Error msg) {
+		faultMessage = msg;
+	}
+
+	public com.medicare.types.Error getFaultMessage() {
+		return faultMessage;
+	}
 }
-    
