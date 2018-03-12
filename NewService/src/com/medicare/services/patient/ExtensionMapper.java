4c558a102e2d615a9c773ead10cc0a9a8660b3d2
maritaria <bramkamies@gmail.com>
2018-03-12 11:32:45 +0100

    Formatting again

--- NewService/src/com/medicare/services/patient/ExtensionMapper.java
+++ NewService/src/com/medicare/services/patient/ExtensionMapper.java
@@ -6,103 +6,67 @@
  * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:04:10 GMT)
  */
 
-        
-            package com.medicare.services.patient;
-        
-            /**
-            *  ExtensionMapper class
-            */
-            @SuppressWarnings({"unchecked","unused"})
-        
-        public  class ExtensionMapper{
+package com.medicare.services.patient;
 
-          public static java.lang.Object getTypeObject(java.lang.String namespaceURI,
-                                                       java.lang.String typeName,
-                                                       javax.xml.stream.XMLStreamReader reader) throws java.lang.Exception{
+/**
+ * ExtensionMapper class
+ */
+@SuppressWarnings({ "unchecked", "unused" })
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "Treatment".equals(typeName)){
-                   
-                            return  com.medicare.types.Treatment.Factory.parse(reader);
-                        
+public class ExtensionMapper {
 
-                  }
+	public static java.lang.Object getTypeObject(java.lang.String namespaceURI, java.lang.String typeName,
+			javax.xml.stream.XMLStreamReader reader) throws java.lang.Exception {
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "ScheduledTreatment".equals(typeName)){
-                   
-                            return  com.medicare.types.ScheduledTreatment.Factory.parse(reader);
-                        
+		if ("http://medicare.com/Types".equals(namespaceURI) && "Treatment".equals(typeName)) {
 
-                  }
+			return com.medicare.types.Treatment.Factory.parse(reader);
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "Doctor".equals(typeName)){
-                   
-                            return  com.medicare.types.Doctor.Factory.parse(reader);
-                        
+		}
 
-                  }
+		if ("http://medicare.com/Types".equals(namespaceURI) && "ScheduledTreatment".equals(typeName)) {
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "Patient".equals(typeName)){
-                   
-                            return  com.medicare.types.Patient.Factory.parse(reader);
-                        
+			return com.medicare.types.ScheduledTreatment.Factory.parse(reader);
 
-                  }
+		}
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "Person".equals(typeName)){
-                   
-                            return  com.medicare.types.Person.Factory.parse(reader);
-                        
+		if ("http://medicare.com/Types".equals(namespaceURI) && "Doctor".equals(typeName)) {
 
-                  }
+			return com.medicare.types.Doctor.Factory.parse(reader);
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "Timeslot".equals(typeName)){
-                   
-                            return  com.medicare.types.Timeslot.Factory.parse(reader);
-                        
+		}
 
-                  }
+		if ("http://medicare.com/Types".equals(namespaceURI) && "Patient".equals(typeName)) {
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "requirements_type0".equals(typeName)){
-                   
-                            return  com.medicare.types.Requirements_type0.Factory.parse(reader);
-                        
+			return com.medicare.types.Patient.Factory.parse(reader);
 
-                  }
+		}
 
-              
-                  if (
-                  "http://medicare.com/Types".equals(namespaceURI) &&
-                  "Room".equals(typeName)){
-                   
-                            return  com.medicare.types.Room.Factory.parse(reader);
-                        
+		if ("http://medicare.com/Types".equals(namespaceURI) && "Person".equals(typeName)) {
 
-                  }
+			return com.medicare.types.Person.Factory.parse(reader);
 
-              
-             throw new org.apache.axis2.databinding.ADBException("Unsupported type " + namespaceURI + " " + typeName);
-          }
+		}
 
-        }
-    
+		if ("http://medicare.com/Types".equals(namespaceURI) && "Timeslot".equals(typeName)) {
+
+			return com.medicare.types.Timeslot.Factory.parse(reader);
+
+		}
+
+		if ("http://medicare.com/Types".equals(namespaceURI) && "requirements_type0".equals(typeName)) {
+
+			return com.medicare.types.Requirements_type0.Factory.parse(reader);
+
+		}
+
+		if ("http://medicare.com/Types".equals(namespaceURI) && "Room".equals(typeName)) {
+
+			return com.medicare.types.Room.Factory.parse(reader);
+
+		}
+
+		throw new org.apache.axis2.databinding.ADBException("Unsupported type " + namespaceURI + " " + typeName);
+	}
+
+}
